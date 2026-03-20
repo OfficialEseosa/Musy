3add499f1b95981f96848e7398f87d6cbe90dfbf
Raphael Omorose <raphaelomorose@gmail.com>
2026-03-20 17:50:00 -0400

    fix: wire up seed questions on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'services/database_helper.dart';
 import 'services/theme_controller.dart';
 import 'screens/home_screen.dart';
 import 'screens/leaderboard_screen.dart';
 import 'screens/question_manager_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseHelper.instance.seedDefaultQuestions();
   runApp(const MusyApp());
 }
 
